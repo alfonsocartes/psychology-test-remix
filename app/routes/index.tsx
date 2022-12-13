9149579743dbeeb0fa1afe278bd71e1008d687e3
Alfonso Cartes <3504951+alfonsocartes@users.noreply.github.com>
2022-12-13 19:46:34 +0100

    style: add tailwind main style

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,7 @@
 export default function Index() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Welcome to Psychology Test
-      </h1>
+    <div className="">
+      <h1 className="text-3xl font-bold">Welcome to Psychology Test</h1>
       <p>Are you an introvert or an extrovert?</p>
       <button>Start test!</button>
     </div>
